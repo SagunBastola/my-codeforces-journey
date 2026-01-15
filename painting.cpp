fc58932eaaf2e1b2f5e706a972d98c252f5fb4e0
Sagun Bastola <sagunbastola34@gmail.com>
2026-01-15 12:53:44 +0545

    square_counting.cpp

---
+++ painting.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        vector<ll> vec(n);
+        set<ll> s;
+        for(ll& x : vec)
+        {
+            cin>>x;
+            s.insert(x);
+        }
+        
+    }
+}
