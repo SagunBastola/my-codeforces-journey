48b5d28c78dc5b8db0b2a00f547366b3c531032e
Sagun Bastola <sagunbastola34@gmail.com>
2026-01-26 17:45:55 +0545

    oly_ans_game_array.cpp

--- olya_and_game_array.cpp
+++ olya_and_game_array.cpp
@@ -1,12 +1,39 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-using ll=long long;
 int main()
 {
-    ll t;
-    cin>>t;
-    while(t--)
+    int t;
+    cin >> t;
+    while (t--)
     {
-        
+        int n;
+        cin >> n;
+        vector<vector<int>> vec(n);
+        for (int i = 0; i < n; i++)
+        {
+            int a;
+            cin >> a;
+            vector<int> vc(a);
+            for (int j = 0; j < a; j++)
+            {
+                cin >> vc[j];
+            }
+            sort(vc.begin(), vc.end());
+            vec[i] = vc;
+        }
+        sort(vec.begin(), vec.end());
+        int sum = vec[0][0];
+        for (int i = 1; i < n; i++)
+        {
+            if (vec[i].size() > 1)
+            {
+                sum += vec[i][1];
+            }
+            else
+            {
+                sum += vec[i][0];
+            }
+        }
+        cout << sum << endl;
     }
 }
