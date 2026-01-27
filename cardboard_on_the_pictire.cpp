7f5a39d83e8cd97ac66204ebf99afbf75f1818d9
Sagun Bastola <sagunbastola34@gmail.com>
2026-01-27 17:46:01 +0545

    card_bonard.cpp

--- cardboard_on_the_pictire.cpp
+++ cardboard_on_the_pictire.cpp
@@ -1,52 +1,56 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+//copied
 using namespace std;
-using ll = long long;
-using ull=unsigned long long;
-int main()
-{
-    ll t;
-    cin >> t;
-    while (t--)
-    {
-        ll n, c;
-        cin >> n >> c;
-        vector<ll> vec(n);
-        ll sum = 0;
-        ll sq = 0;
-        for (ll &x : vec)
-        {
-            cin >> x;
-        }
-        ull low = 0;
-        ull high = 1e7;
-        ull ans = 0;
-        while (low <= high)
-        {
-            ull mid = low + (high - low) / 2;
-            ull total_area = 0;
+typedef unsigned long long ull;
 
-            for (int i = 0; i < n; i++)
-            {
-                ull side = vec[i] + 2 * mid;
-                total_area += side * side;
-                if (total_area > c)
-                    break;
-            }
+void solve() {
+    ull n, c;
+    cin >> n >> c;
+    vector<ull> s(n);
+    for (int i = 0; i < n; i++) cin >> s[i];
 
-            if (total_area == c)
-            {
-                ans = mid;
+    ull low = 1; ull high = 1e19; 
+    ull ans = 0;
+
+    while (low <= high) {
+        ull mid = low + (high - low) / 2;
+        ull total_area = 0;
+        bool too_big = false;
+
+        for (int i = 0; i < n; i++) {
+            ull side = s[i] + mid;
+            
+            // Check for multiplication overflow or total sum overflow
+            if (side > 2e9 || (side * side) > c) { 
+                too_big = true;
                 break;
             }
-            else if (total_area > c)
-            {
-                high = mid - 1;
-            }
-            else
-            {
-                low = mid + 1;
+            
+            total_area += (side * side);
+            if (total_area > c) {
+                too_big = true;
+                break;
             }
         }
-        cout << ans << endl;
+
+        if (total_area == c) {
+            ans = mid;
+            break;
+        } else if (too_big || total_area > c) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
     }
+    cout << ans/2 << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
 }
