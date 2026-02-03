f404f19d72b85dae4e7c84c74e34b8e859b9b5d2
Sagun Bastola <sagunbastola34@gmail.com>
2026-02-03 09:18:28 +0545

    ehad_and_aahd.cpp

---
+++ ehad_and_aahd.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        cout<<n-1<<" "<<1<<endl;
+    }
+}
