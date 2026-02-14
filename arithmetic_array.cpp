17fe2a5857580e44e06ae472d633c3cc76a6dc83
Sagun Bastola <sagunbastola34@gmail.com>
2026-02-14 17:48:04 +0545

    arithmetic_array.cpp

---
+++ arithmetic_array.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+#define rep(i, m, n) for (ll i = m; i < n; i++)
+#define repr(i, m, n) for (ll i = m; i >= 0; i--)
+template<typename T>
+istream& operator>>(istream &s, vector<T> &v)
+{
+    for(auto &x : v)
+        s >> x;
+    return s;
+}
+#define vi vector<int>
+#define pi pair<int>
+void solve()
+{
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
--- gurilla_and_permutation.cpp
+++ gurilla_and_permutation.cpp
@@ -2,18 +2,29 @@
 using ll = long long;
 using namespace std;
 #define rep(i, m, n) for (ll i = m; i < n; i++)
-#define repr(i, m, n) for (ll i = m; i >= 0; i--)
-template<typename T>
-istream& operator>>(istream &s, vector<T> &v)
+#define repr(i, m, n) for (ll i = m; i >= n; i--)
+template <typename T>
+istream &operator>>(istream &s, vector<T> &v)
 {
-    for(auto &x : v)
+    for (auto &x : v)
         s >> x;
     return s;
 }
-#define vi vector<int>
-#define pi pair<int>
+#define vi vector<ll>
+#define pi pair<ll>
 void solve()
 {
+    ll n,m,k;
+    cin>>n>>m>>k;
+    repr(i,n,m+1)
+    {
+        cout<<i<<" ";
+    }
+    rep(i,1,m+1)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
 }
 int main()
 {
