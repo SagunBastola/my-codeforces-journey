e803dbe66846f0b7669b641d2682041f0a38e59d
Sagun Bastola <sagunbastola34@gmail.com>
2026-06-16 21:47:22 +0545

    beautiful_sequence.cpp

---
+++ beautiful_sequence.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+#define rep(i, m, n) for (ll i = m; i < n; i++)
+#define repr(i, m, n) for (ll i = m; i >= n; i--)
+template<typename T>
+istream& operator>>(istream &s, vector<T> &v)
+{
+    for(auto &x : v)
+        s >> x;
+    return s;
+}
+template<typename T>
+ostream& operator<<(ostream& s,vector<T> &v)
+{
+    for(auto &x : v)
+    {   
+        s<<x<<" ";
+    }
+    return s;
+}
+#define vi vector<ll>
+#define pi pair<ll,ll>
+void solve()
+{
+    ll n;
+    cin>>n;
+
+    vi vec(n);
+    cin>>vec;
+
+    for(ll i=0;i<n;i++)
+    {
+        if(vec[i] <= (i+1))
+        {
+            cout<<"YES"<<endl;
+            return;
+        }
+    }
+    cout<<"NO"<<endl;
+
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
