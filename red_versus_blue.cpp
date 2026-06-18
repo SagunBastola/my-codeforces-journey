dd934662353d999c5da71d5db7011ce1c8c4745e
Sagun Bastola <sagunbastola34@gmail.com>
2026-06-18 14:32:03 +0545

    red_Versus_blue.cpp

--- red_versus_blue.cpp
+++ red_versus_blue.cpp
@@ -1,13 +1,68 @@
 #include <bits/stdc++.h>
-using namespace std;
 using ll = long long;
-
-int main() {
+using namespace std;
+#define rep(i, m, n) for (ll i = m; i < n; i++)
+#define repr(i, m, n) for (ll i = m; i >= n; i--)
+template <typename T>
+istream &operator>>(istream &s, vector<T> &v)
+{
+    for (auto &x : v)
+        s >> x;
+    return s;
+}
+template <typename T>
+ostream &operator<<(ostream &s, vector<T> &v)
+{
+    for (auto &x : v)
+    {
+        s << x << " ";
+    }
+    return s;
+}
+#define vi vector<ll>
+#define pi pair<ll, ll>
+void solve()
+{
+    float n, r, b;
+    cin >> n >> r >> b;
+    ll cn = round(r / (b + 1));
+    ll temp = cn;
+    string s = "";
+    while (temp > 0 && r > 0)
+    {
+        s += 'R';
+        r--;
+        temp--;
+    }
+    s += 'B';
+    b--;
+    while (r > 0 && b > 0)
+    {
+        temp = round(r / (b + 1));
+        while (temp > 0 && r > 0)
+        {
+            s += 'R';
+            r--;
+            temp--;
+        }
+        s+='B';
+        b--;
+    }
+    while (r > 0)
+    {
+        s += 'R';
+        r--;
+    }
+    cout << s << endl;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     int t;
     cin >> t;
-    while (t--) {
-        ll n, r, b;
-        cin >> n >> r >> b;
-        
+    while (t--)
+    {
+        solve();
     }
 }
